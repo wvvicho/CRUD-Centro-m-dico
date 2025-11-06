ef699ce57eecba7b943f70932c4e57e0a02aa219
wvvicho <vicente.rosales05@gmail.com>
2025-11-06 17:09:36 -0300

    Creación de models + migración base de datos

--- CentroMedico/models.py
+++ CentroMedico/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Especialidad (models.Model):
+    nombre = models.CharField(max_length=15)
+
+class Medico (models.Model):
+    nombre_completo = models.CharField(max_length=70)
+    rut = models.CharField(unique=True, max_length=10)
+    especialidad = models.ForeignKey(Especialidad, on_delete=models.CASCADE, related_name='medicos')
+    correo = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=15, null=True, blank=True)
+
+class Paciente (models.Model):
+    nombre_completo = models.CharField(max_length=70)
+    rut = models.CharField(unique=True, max_length=10)
+    fecha_nacimiento = models.DateField(null=False, blank=False)
+    sexo = models.CharField(max_length=1)
+    telefono = models.CharField(max_length=15, null=True, blank=True)
+
+class Cita (models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='citas')
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE, related_name='citas')
+    especialidad = models.ForeignKey(Especialidad, on_delete=models.CASCADE, related_name='citas')
+    fecha_cita = models.DateField()
+    hora_cita = models.TimeField()
+    observaciones = models.TextField(null=True, blank=True)
