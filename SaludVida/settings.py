796242898555b51d5ce3e6782eafa6a42c2bd2fc
wvvicho <vicente.rosales05@gmail.com>
2025-11-06 16:52:39 -0300

    Ajustando configuraciones del proyecto

--- SaludVida/settings.py
+++ SaludVida/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CentroMedico',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'SaludVida.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['SaludVida/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'SaludVida.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'CentroMedico',
+        'USER':'adminSalud',
+        'PASSWORD':'SaludVida',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
