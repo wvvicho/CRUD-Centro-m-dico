91809317545bbac88cccc165e0c470679198dd3d
wvvicho <vicente.rosales05gmail.com>
2025-11-06 20:16:41 -0300

    Arreglo de archivo SQL al momento de crear usuario

--- centroMedico.sql
+++ centroMedico.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE IF NOT EXISTS CentroMedico;
-CREATE USER 'adminSalud'@'localhost' IDENTIFIED BY 'SaludVida';
+CREATE USER IF NOT EXISTS 'adminSalud'@'localhost' IDENTIFIED BY 'SaludVida';
 GRANT ALL PRIVILEGES ON CentroMedico.* TO 'adminSalud'@'localhost'; 
